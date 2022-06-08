a14be3d49cb07813ef8a73e085db12d5f3a8272b
Kumar02 <adesh.kumar02@mind.motherson.com>
2022-06-08 11:42:49 +0530

    Initial commit

---
+++ src/main/java/com/motherson/dna/my_app/App.java
@@ -0,0 +1,14 @@
+package com.motherson.dna.my_app;
+
+/**
+ * Hello world!
+ *
+ * modified main() to display args[0]
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World ! "+args[0] );
+    }
+}
